b56da66e6dc2cf96a13a20f291a8b429f74e70eb
Maksym Hladkoskok <gladkoskokmaks@gmail.com>
2024-03-09 14:08:13 +0100

    Change package and pamateres names

--- CrossPlatform/Laba1/SimpleCalculator/untitled/src/Calculator.java
+++ CrossPlatform/Laba1/SimpleCalculator/untitled/src/Calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-pпше ublic class Calculator {
+public class Calculator {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
